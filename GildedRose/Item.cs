95eb41e16cb9245bdcb7777c500bc605e8dfd733
local-user <local@mail.com>
2023-06-02 17:13:27 +0200

    restored Item class, edit is against goblin rqrmnt

--- GildedRose/Item.cs
+++ GildedRose/Item.cs
@@ -1,26 +1,9 @@
-﻿namespace GildedRoseKata;
-
-public class Item
+﻿namespace GildedRoseKata
 {
-    private int quality;
-
-    public string? Name { get; set; }
-
-    public int SellIn { get; set; }
-
-    public int Quality
+    public class Item
     {
-        get { return quality; }
-        set
-        {
-            if (Name != null && Name == "Sulfuras")
-            {
-                quality = 80; // Sulfuras is legendary and its quality is always 80
-            }
-            else
-            {
-                quality = value > 50 ? 50 : value; // Quality capped at 50
-            }
-        }
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
     }
 }
