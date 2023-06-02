060202968ca88cfda5075e25d84c61eb865e2583
local-user <local@mail.com>
2023-06-02 16:48:44 +0200

    cleaned "if" syntax, logic unchanged

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -4,7 +4,7 @@ namespace GildedRoseKata;
 
 public class GildedRose
 {
-    IList<Item> Items;
+    readonly IList<Item> Items;
     public GildedRose(IList<Item> Items)
     {
         this.Items = Items;
@@ -16,44 +16,32 @@ public class GildedRose
         {
             if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (Items[i].Quality > 0)
+                if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
+                    Items[i].Quality--;
                 }
             }
-            else
+            else if (Items[i].Quality < 50)
             {
-                if (Items[i].Quality < 50)
+                Items[i].Quality++;
+
+                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    Items[i].Quality = Items[i].Quality + 1;
-
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (Items[i].SellIn < 11 && Items[i].Quality < 50)
                     {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
+                        Items[i].Quality++;
+                    }
 
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
+                    if (Items[i].SellIn < 6 && Items[i].Quality < 50)
+                    {
+                        Items[i].Quality++;
                     }
                 }
             }
 
             if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
             {
-                Items[i].SellIn = Items[i].SellIn - 1;
+                Items[i].SellIn--;
             }
 
             if (Items[i].SellIn < 0)
@@ -62,25 +50,19 @@ public class GildedRose
                 {
                     if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Quality > 0)
+                        if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
+                            Items[i].Quality--;
                         }
                     }
                     else
                     {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        Items[i].Quality -= Items[i].Quality;
                     }
                 }
-                else
+                else if (Items[i].Quality < 50)
                 {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
+                    Items[i].Quality++;
                 }
             }
         }
