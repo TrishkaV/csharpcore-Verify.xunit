c930b5ce07a3127e889cbe4cc2c0a6022209b393
local-user <local@mail.com>
2023-06-02 16:24:04 +0200

    cleaned syntax, removed unused args

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GildedRoseKata
+namespace GildedRoseKata;
+public static class Program
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("OMGHAI!");
+        Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
+        IList<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
@@ -37,20 +36,18 @@ namespace GildedRoseKata
 				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
-            var app = new GildedRose(Items);
+        var app = new GildedRose(Items);
 
-
-            for (var i = 0; i < 31; i++)
+        for (var i = 0; i < 31; i++)
+        {
+            Console.WriteLine("-------- day " + i + " --------");
+            Console.WriteLine("name, sellIn, quality");
+            for (var j = 0; j < Items.Count; j++)
             {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
-                {
-                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
-                }
-                Console.WriteLine("");
-                app.UpdateQuality();
+                Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
             }
+            Console.WriteLine("");
+            app.UpdateQuality();
         }
     }
 }
